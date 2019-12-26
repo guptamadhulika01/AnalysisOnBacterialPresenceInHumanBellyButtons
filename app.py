23297339f33f2135749bf111a23333d4caefee1d
Madhulika Gupta <guptamadhulika01@gmail.com>
2019-12-26 09:45:11 -0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import sqlalchemy;
 from sqlalchemy.ext.automap import automap_base;
 from sqlalchemy.orm import Session;
 from sqlalchemy import create_engine;
-
+#Import Flask, Jsonify and Render templates
 from flask import Flask, jsonify, render_template;
 from flask_sqlalchemy import SQLAlchemy;
 
